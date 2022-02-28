4dfde9214292aaca0997111f9d14440e359967a0
rajndev <rajnsoftwaredev@gmail.com>
2022-02-28 12:13:04 -0500

    Bug Fix: Use refs to return correct value from input field

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,58 +4,67 @@ import Keypad from './Keypad'
 import * as math from 'mathjs'
 
 class Calculator extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-     displayValue: "",
-    };
-  }
-
-  handleKeyClick(i) {
-    this.setState({displayValue: this.state.displayValue.concat(i)});
-
-   /*  if(this.state.displayValue != ""){
-      let fdsa = this.state.displayValue.length % 2
-      if(fdsa == 0){
-        this.setState({displayValue: this.state.displayValue.concat(i + '\n')});
-      } */
-    
-  }
-
-  handleEqualsClick() {
-    
-    //let trimmed = this.state.displayValue.replace(/[\n\r]+/g, '')
-    let result;
-
-    try {
-      result = math.evaluate(this.state.displayValue);
+    constructor(props) {
+        super(props);
+        this.state = {
+            //  displayValue: "",
+            runningValue: "",
+            calcResult: 0
+        };
+        this.inputFieldRef = React.createRef(null);
     }
-    catch{
-       return; 
+
+    handleKeyClick(i) {
+        //this.setState({runningValue: this.state.runningValue.concat(i)});
+        // console.log(this.state.runningValue);
     }
-    this.setState({displayValue: result});
-  }
 
-  handleClearClick() {
-    this.setState({displayValue: ""});
-  }
+    setInputRef(inputRef) {
+        this.setState({ inputRef: inputRef.current });
+    }
 
-  handleBackClick() {
-    let sliced = this.state.displayValue.slice(0, -1);
-    this.setState({displayValue: sliced});
-  }
+    handleInputChange() {
+        console.log(this.inputFieldRef.current.value)
+        // let homer = this.textInput.value;
+        // this.setState({ runningValue: homer });
+        //console.log(this.state.runningValue);
+    }
 
-  render() {
-    return (
-      <div className="container">
-        <Display dispValue={this.state.displayValue}/>
-        <Keypad onKeyClick={i => this.handleKeyClick(i)} 
-                onEqualsKeyClick={() => this.handleEqualsClick()} 
-                onClearKeyClick={() => this.handleClearClick()} 
-                onBackKeyClick={() => this.handleBackClick()}/>
-      </div>
-    )
-  }
+    handleEqualsClick() {
+        let result = 0;
+        let running = this.state.runningValue;
+
+        try {
+            result = math.evaluate(running);
+            this.setState({ calcResult: result });
+            // console.log(calcResult);
+            //this.setState({result: result});
+        }
+        catch {
+            return;
+        }
+    }
+
+    handleClearClick() {
+        this.setState({ runningValue: "" });
+    }
+
+    handleBackClick() {
+        let sliced = this.state.runningValue.slice(0, -1);
+        this.setState({ runningValue: sliced });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Display result={this.state.calcResult} inputRef={this.inputFieldRef} onInputChange={() => this.handleInputChange()} />
+                <Keypad onKeyClick={i => this.handleKeyClick(i)}
+                    onEqualsKeyClick={() => this.handleEqualsClick()}
+                    onClearKeyClick={() => this.handleClearClick()}
+                    onBackKeyClick={() => this.handleBackClick()} />
+            </div>
+        )
+    }
 }
 
 export default Calculator;
