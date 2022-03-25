d8510283317a18fc731b20ce78a38199df895349
rajndev <rajnsoftwaredev@gmail.com>
2022-03-25 17:40:12 -0400

    Limit keyboard character count and Redo keypad UI

--- src/components/keypad/Keypad.js
+++ src/components/keypad/Keypad.js
@@ -6,30 +6,34 @@ class Keypad extends Component {
         return (
             <div className="keys">
                 <div className="row">
-                    <button name="C" className="helper" onClick={(e) => this.props.onClearKeyClick(e.target.name)}>C</button>
-                    <button name="%" className="helper" onClick={(e) => this.props.onKeyClick(e.target.name)}>%</button>
-                    <button name="()" className="helper" onClick={(e) => this.props.onKeyClick(e.target.name)}>( )</button>
-                    <button name="+" className="operator" onClick={(e) => this.props.onKeyClick(e.target.name)}>+</button>
-
-                    <button name="7"  onClick={(e) => this.props.onKeyClick(e.target.name)}>7</button>
-                    <button name="8"  onClick={(e) => this.props.onKeyClick(e.target.name)}>8</button>
-                    <button name="9"  onClick={(e) => this.props.onKeyClick(e.target.name)}>9</button>
-                    <button name="-" className="operator" onClick={(e) => this.props.onKeyClick(e.target.name)}>-</button>
-
-                    <button name="4"  onClick={(e) => this.props.onKeyClick(e.target.name)}>4</button>
-                    <button name="5"  onClick={(e) => this.props.onKeyClick(e.target.name)}>5</button>
-                    <button name="6"  onClick={(e) => this.props.onKeyClick(e.target.name)}>6</button>
-                    <button name="*" className="operator" onClick={(e) => this.props.onKeyClick(e.target.name)}>x</button>
-
-                    <button name="1"  onClick={(e) => this.props.onKeyClick(e.target.name)}>1</button>
-                    <button name="2"  onClick={(e) => this.props.onKeyClick(e.target.name)}>2</button>
-                    <button name="3"  onClick={(e) => this.props.onKeyClick(e.target.name)}>3</button>
-                    <button name="/" className="operator" onClick={(e) => this.props.onKeyClick(e.target.name)}>÷</button>
-
-                    <button name="0"  onClick={(e) => this.props.onKeyClick(e.target.name)}>0</button>
-                    <button name="."  onClick={(e) => this.props.onKeyClick(e.target.name)}>.</button>
-                    <button name="←"  onClick={(e) => this.props.onBackKeyClick(e.target.name)}>←</button>
-                    <button name="=" className="equals-key" onClick={(e) => this.props.onEqualsKeyClick(e)}>=</button>
+                    <button name="C" className="helper rounded-circle" onClick={(e) => this.props.onClearKeyClick(e.target.name)}>C</button>
+                    <button name="%" className="helper rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>%</button>
+                    <button name="()" className="helper rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>( )</button>
+                    <button name="&#44;" className="operator rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>&#43;</button>
+                </div>
+                <div className="row">
+                    <button name="7"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>7</button>
+                    <button name="8"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>8</button>
+                    <button name="9"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>9</button>
+                    <button name="-" className="operator rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>-</button>
+                </div>
+                <div className="row">
+                    <button name="4"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>4</button>
+                    <button name="5"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>5</button>
+                    <button name="6"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>6</button>
+                    <button name="*" className="operator rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>x</button>
+                </div>    
+                <div className="row">
+                    <button name="1"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>1</button>
+                    <button name="2"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>2</button>
+                    <button name="3"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>3</button>
+                    <button name="/" className="operator rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>÷</button>
+                </div>  
+                <div className="row">
+                    <button name="0"  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>0</button>
+                    <button name="."  className="rounded-circle" onClick={(e) => this.props.onKeyClick(e.target.name)}>.</button>
+                    <button name="←"  className="rounded-circle" onClick={(e) => this.props.onBackKeyClick(e.target.name)}>←</button>
+                    <button name="=" className="equals-key rounded-circle" onClick={(e) => this.props.onEqualsKeyClick(e)}>=</button>
                 </div>
             </div>
         )
